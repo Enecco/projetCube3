baaf90ed12719075d7d722d6631bb2cfdcfad148
pikate <q.picat@gmail.com>
2021-08-29 15:22:06 +0200

    new

--- API/Application/index.php
+++ API/Application/index.php
@@ -1,6 +1,6 @@
 
 <?php
-// CA FONCTIONNE BIENNNNN
+// CA FONCTIONNE BIENNNNN pour sur
 // chemin ABSOLU | fichier index = routeur |
 // Constante définie permettant de crer le chemin absolu pr l'acces à nos ressources (img, code etc)
 // la constante URL sera remplacé par notre index.php avec devant hhtps / http suivi de mon API
