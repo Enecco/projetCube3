b4cde5c10a1a53a7d296e3d1a2273b70d2d48b8d
pikate <76156331+pikate@users.noreply.github.com>
2021-08-29 12:39:47 +0200

    Update index.php

--- API/Application/index.php
+++ API/Application/index.php
@@ -1,6 +1,6 @@
 
 <?php
-
+// bonsoir
 // chemin ABSOLU | fichier index = routeur |
 // Constante définie permettant de crer le chemin absolu pr l'acces à nos ressources (img, code etc)
 // la constante URL sera remplacé par notre index.php avec devant hhtps / http suivi de mon API
