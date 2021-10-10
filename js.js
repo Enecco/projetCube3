8eccb945d98d175d81f96467d302f649ec74eba4
pikate <q.picat@gmail.com>
2021-10-10 21:00:40 +0200

    many changes + page contact

--- js.js
+++ js.js
@@ -2,10 +2,14 @@ let formRecherche = document.querySelector('#recherche');
 
 let formMenu = document.querySelector('#menu');
 let submitCatalogue = document.getElementById('submitCatalogue');
-let submitContact = document.querySelector('#submitContact');
+let submitContact = document.getElementById('submitContact');
 
 submitCatalogue.addEventListener('click', (e) => {
     e.preventDefault();
     window.location.href = "catalogue.html";
 });
 
+submitContact.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = "contact.html";
+});
