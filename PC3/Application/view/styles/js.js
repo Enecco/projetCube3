2ee7a209c427a647554d2633f303bb702de742d1
pikate <76156331+pikate@users.noreply.github.com>
2021-10-14 17:16:16 +0200

    recup value des input en JS

--- PC3/Application/view/styles/js.js
+++ PC3/Application/view/styles/js.js
@@ -2,22 +2,43 @@ let espaceUser = document.querySelector("#espaceUser");
 let nomEspaceUser = document.querySelector("#nomEspaceUser");
 let espaceGuest = document.querySelector("#espaceGuest");
 let nomEspaceGuest = document.querySelector("#nomEspaceGuest");
+let logged = false;
 
-espaceUser.style.display = "none";
+if(!logged){
+    espaceUser.style.display = "none";
+}
+
 
 let btnInscription = document.querySelector("#btnInscription");
 let btnConnexion = document.querySelector("#btnConnexion");
 let pageConnexion = document.querySelector('#pageConnexion');
 let pageInscription = document.querySelector('#pageInscription');
+let inscriptionEmail = document.querySelector('#inscriptionEmail');
+let envoiConnexion = document.querySelector('#envoiConnexion');
 
+
+if(envoiConnexion){
+    inscriptionEmail
+    envoiConnexion.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (inscriptionEmail.value === '' || inscriptionPassword.value === '') {
+            alert('Rempli les champs mail ET mdp');
+        }
+        else{
+            alert('Tu as bien rempli les champs, bg');
+        }
+});}
+
+if(btnConnexion){
 btnConnexion.addEventListener('click', () => {
       pageConnexion.style.display = 'block';
       pageInscription.style.display = 'none';
-});
+});}
 
+if(btnInscription){
 btnInscription.addEventListener('click', () => {
     pageInscription.style.display = 'block';
     pageConnexion.style.display = 'none';
-});
+});}
 
 
