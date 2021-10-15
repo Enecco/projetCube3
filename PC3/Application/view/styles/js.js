cb30466142d868e52255ddd5cd7906b758b5bd07
pikate <76156331+pikate@users.noreply.github.com>
2021-10-15 09:11:07 +0200

    diff connexion/inscription connexion.html

--- PC3/Application/view/styles/js.js
+++ PC3/Application/view/styles/js.js
@@ -15,30 +15,42 @@ let pageConnexion = document.querySelector('#pageConnexion');
 let pageInscription = document.querySelector('#pageInscription');
 let inscriptionEmail = document.querySelector('#inscriptionEmail');
 let envoiConnexion = document.querySelector('#envoiConnexion');
-
-
-if(envoiConnexion){
-    inscriptionEmail
-    envoiConnexion.addEventListener('click', (e) => {
-        e.preventDefault();
-        if (inscriptionEmail.value === '' || inscriptionPassword.value === '') {
-            alert('Rempli les champs mail ET mdp');
-        }
-        else{
-            alert('Tu as bien rempli les champs, bg');
-        }
-});}
+let mode = 'inscription';
 
 if(btnConnexion){
 btnConnexion.addEventListener('click', () => {
       pageConnexion.style.display = 'block';
       pageInscription.style.display = 'none';
+      mode = 'connexion';
 });}
 
 if(btnInscription){
 btnInscription.addEventListener('click', () => {
     pageInscription.style.display = 'block';
     pageConnexion.style.display = 'none';
+    mode = 'inscription';
 });}
 
+if((envoiConnexion)){
+    envoiConnexion.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (mode === 'inscription') {
 
+            if (inscriptionEmail.value === '' || inscriptionPassword.value === '') {
+                alert('Inscription : Rempli les champs mail ET mdp');
+            }
+            else{
+                alert('Inscription : Tu as bien rempli les champs, bg');
+            }
+
+        }
+        else if(mode === 'connexion'){
+
+            if (connexionEmail.value === '' || connexionPassword.value === '') {
+                alert('Connexion : Tu n\'as pas indiqué ton mail ou ton mdp');
+            }
+            else{
+                alert('Connexion : Tu as bien rempli les champs, tu vas être connecté');
+            }
+        }
+});}
