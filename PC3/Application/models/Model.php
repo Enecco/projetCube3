8f7e6784dc659ce67b4b74201242562ca4d223d6
pikate <q.picat@gmail.com>
2021-10-25 19:13:50 +0200

    try new model.php

--- PC3/Application/models/Model.php
+++ PC3/Application/models/Model.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
     $servername = "localhost";
     $username = "enekatestream";
     $password = "enekatestream33";
@@ -7,4 +7,33 @@
     $pdo = new mysqli( $servername, $username, $password, $dbname);
     if($pdo->connect_error) {
         die("Connection Failed" . $pdo->connect_error);
+    }?> -->
+
+<?php
+abstract class Model {
+    private static $pdo;
+
+    private static function setBdd(){
+        self::$pdo = new PDO("mysql:host=localhost;dbname=projetraspberry;charset=utf8","root","");
+        self::$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
     }
+
+    protected function getBdd(){
+        if(self::$pdo === null){
+            self::setBdd();
+        }
+        return self::$pdo;
+    }
+
+    public static function sendJSON(
+        $info
+        ){
+        header('Access-Control-Allow-Origin: *');
+        // header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        // header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+        header('Access-Control-Allow-Credentials: true');
+      
+       header('Content-Type: application/json');
+      echo json_encode($info);  
+    }
+}
