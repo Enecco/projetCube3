bfbdcf5a7919cc769956980d8dcea6f7fb8e466b
Enecco Jacobelli <eneccojacobelli@gmail.com>
2021-11-11 20:34:30 +0100

    changement de back -> mobile

---
+++ PC3/Application/controllers/back/contact.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'headers.php';
+
+$conn = new mysqli('localhost', 'root', '', 'enekatestream');
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  if (isset($_GET['id'])) {
+    $id = $conn->real_escape_string($_GET['id']);
+    $sql = $conn->query("SELECT * FROM `contact` WHERE `ID_contact` = '$id'");
+    $data = $sql->fetch_assoc();
+  } else {
+    $data = array();
+    $sql = $conn->query("SELECT * FROM contact");
+    while ($d = $sql->fetch_assoc()) {
+      $data[] = $d;
+    }
+  }
+
+  exit(json_encode($data));
+}
---
+++ PC3/Application/controllers/back/contenu.php
@@ -0,0 +1,58 @@
+<?php
+require_once 'headers.php';
+
+$conn = new mysqli('localhost', 'root', '', 'enekatestream');
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  if (isset($_GET['id'])) {
+    $id = $conn->real_escape_string($_GET['id']);
+    $sql = $conn->query("SELECT * FROM `contenu` WHERE `ID_contenu` = '$id'");
+    $data = $sql->fetch_assoc();
+  } else {
+    $data = array();
+    $sql = $conn->query("SELECT * FROM contenu");
+    while ($d = $sql->fetch_assoc()) {
+      $data[] = $d;
+    }
+  }
+
+  exit(json_encode($data));
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $data = json_decode(file_get_contents("php://input"));
+  echo $data->titre;
+  $sql = $conn->query("INSERT INTO `contenu` (titre, date_film, image) VALUES ('".$data->titre."', '".$data->date_film."', '".$data->image."')");
+
+  if ($sql) {
+      $data->ID_contenu = $conn->insert_id;
+      exit(json_encode($data));
+  } else {
+      exit(json_encode(array('status' => 'error')));
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+  if (isset($_GET['id'])) {
+    $id = $conn->real_escape_string($_GET['id']);
+    $data = json_decode(file_get_contents("php://input"));
+    $sql = $conn->query("UPDATE `contenu` SET  titre = '".$data->titre."', date_film = '".$data->date_film."', image = '".$data->image."' WHERE `ID_contenu` = '$id'");
+    if ($sql) {
+      exit(json_encode(array('status' => 'success')));
+    } else {
+      exit(json_encode(array('status' => 'error')));
+    }
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+  if (isset($_GET['id'])) {
+    $id = $conn->real_escape_string($_GET['id']);
+    $sql = $conn->query("DELETE FROM `contenu` WHERE `ID_contenu` = '$id'");
+    if ($sql) {
+      exit(json_encode(array('status' => 'success')));
+    } else {
+      exit(json_encode(array('status' => 'error')));
+    }
+  }
+}
